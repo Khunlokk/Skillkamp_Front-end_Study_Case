4790a8c0dc05098832f214eedee9aa7d679102f3
Khunlokk <chaiwat.pancho@gmail.com>
2023-04-11 23:24:08 +0700

    add Our Story

--- happykids/src/Pages/OurStory.js
+++ happykids/src/Pages/OurStory.js
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
+import "./OurStory.css";
 
 function OurStory() {
-  return <div>OurStory</div>;
+  const [images, setImages] = useState([]);
+  useEffect(() => {
+    // Fetch images from API and set state
+    fetch("https://skillkamp-api.com/v1/api/images/story")
+      .then((response) => response.json())
+      .then((data) => setImages(data.detail))
+      .catch((error) => console.error("Error fetching images:", error));
+  }, []);
+  return (
+    <div>
+      <div>
+        <img src={images} className="img_ourstory" />
+      </div>
+      <h1 className="our">OurStory</h1>
+      <p className="story">
+        I'm a paragraph. Click here to add your own text and edit me. It’s easy.
+        Just click “Edit Text” or double click me and you can start adding your
+        own content and make changes to the font. Feel free to drag and drop me
+        anywhere you like on your page. I’m a great place for you to tell a
+        story and let your users know a little more about you.
+      </p>
+      <p className="story">
+        ​This is a great space to write long text about your company and your
+        services. You can use this space to go into a little more detail about
+        your company. Talk about your team and what services you provide. Tell
+        your visitors the story of how you came up with the idea for your
+        business and what makes you different from your competitors. Make your
+        company stand out and show your visitors who you are.
+      </p>
+    </div>
+  );
 }
 
 export default OurStory;
